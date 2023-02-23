7df1686442d36b78ec474b6cafddb84f8eeb1a11
Juan D. Pinto <juan@juandpinto.com>
2023-02-23 08:59:22 -0600

    Fix additional typo

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -7,4 +7,4 @@
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
 #define U_GQT(MOD, TAP) &u_gqt MOD TAP // MY MODIFICATION
 #define U_PHT_LEFT(MOD, TAP) &u_pht_left MOD TAP // MY MODIFICATION
-#define U_PHT_RIGHT(MOD, TAP) &u_pht_RIGHT MOD TAP // MY MODIFICATION
+#define U_PHT_RIGHT(MOD, TAP) &u_pht_right MOD TAP // MY MODIFICATION
