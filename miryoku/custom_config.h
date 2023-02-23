78fcaa51cf54789b8fa6a755ca1c5183fdf66b0c
Juan D. Pinto <juan@juandpinto.com>
2023-02-23 10:50:37 -0600

    Revert changes

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,10 +4,10 @@
 #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
 #define MIRYOKU_LAYER_BASE \
-&kp Q,             &kp W,             &kp F,              &kp P,                         &kp B,              &kp J,             &kp L,                          &kp U,             &kp Y,             &kp SQT,           \
-U_GQT(LGUI, A),    U_GQT(LALT, R),    U_GQT(LCTRL, S),    U_PHT_LEFT(LSHFT, T),          &kp G,              &kp M,             U_PHT_RIGHT(LSHFT, N),          U_GQT(LCTRL, E),   U_GQT(LALT, I),     U_GQT(LGUI, O),     \
-U_LT(U_BUTTON, Z), U_GQT(RALT, X),    &kp C,              U_GQT(LC(LS(LA(LGUI))), D),    &kp V,              &kp K,             U_GQT(LC(LS(LA(LGUI))), H),     &kp COMMA,         U_GQT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC), U_LT(U_NAV, BSPC),             U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),  U_LT(U_NUM, SPC),               U_LT(U_FUN, DEL),  U_NP,               U_NP
+&kp Q,             &kp W,             &kp F,             &kp P,                         &kp B,             &kp J,             &kp L,                          &kp U,             &kp Y,             &kp SQT,           \
+U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),                &kp G,             &kp M,             U_MT(LSHFT, N),                 U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             U_MT(LC(LS(LA(LGUI))), D),     &kp V,             &kp K,             U_MT(LC(LS(LA(LGUI))), H),      &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, BSPC),             U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, SPC),               U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define CONFIG_ZMK_MOUSE
 #define CONFIG_ZMK_SLEEP
--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -5,6 +5,3 @@
 
 #define U_MT(MOD, TAP) &u_mt MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
-#define U_GQT(MOD, TAP) &u_gqt MOD TAP // MY MODIFICATION
-#define U_PHT_LEFT(MOD, TAP) &u_pht_left MOD TAP // MY MODIFICATION
-#define U_PHT_RIGHT(MOD, TAP) &u_pht_right MOD TAP // MY MODIFICATION
